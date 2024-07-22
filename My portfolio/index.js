763ba0378491f050947cea49c2b5a57f27c88f6b
Subhajit <dass8684@gmail.com>
2024-07-22 05:54:43 +0530

    commit

---
+++ My portfolio/index.js
@@ -0,0 +1,16 @@
+
+const myArray = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+const mySecondArray = ["Subhajit", "Abhijit", "Rabijit", "Abhishek"]
+
+console.log(myArray[4]);
+
+console.log(mySecondArray[2], myArray[2]);
+
+ myArray.push(120);
+ myArray.push(140);
+ myArray.unshift(5);
+ myArray.shift(5);
+
+console.log(myArray);
+
